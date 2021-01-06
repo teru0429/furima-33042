6e0d978fd3313796cb5c3d59b71878184b9a5058
teru0429 <75606622+teru0429@users.noreply.github.com>
2021-01-06 18:38:51 +0900

    修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,7 @@
 FactoryBot.define do
   factory :user do
     email {Faker::Internet.free_email}
-    password = Faker::Internet.password(min_length: 6)
-    password {password}
+    password { '1a' + Faker::Internet.password(min_length: 6) }
     password_confirmation {password}
     nickname {"test"}
     last_name {"山田"}
@@ -12,12 +11,3 @@ FactoryBot.define do
     birth {"2000-01-01"}
   end
 end
-
-
-
-# nickname {Faker::Nickname.name}
-#     last_name {Faker::Name.last_name}
-#     first_name {Faker::Name.first_name}
-#     last_name_kana {Faker::Name.last.katakana}
-#     first_name_name {Faker::Name.first.katakana}
-#     barth {Faker::Barth.barth}
