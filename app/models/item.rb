46b02fb13c9f7d54079dcae9191dfd87b0c44e57
teru0429 <75606622+teru0429@users.noreply.github.com>
2021-01-07 18:24:15 +0900

    出品の保存

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,7 +10,7 @@ class Item < ApplicationRecord
   belongs_to :scheduled_delivery
 
   #空の投稿を保存できないようにする
-  validates :name, :info, :price, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, presence: true
+  validates_presence_of :name, :info, :price, :image
 
   #ジャンルの選択が「--」の時は保存できないようにする
   validates :category_id,            numericality: { other_than: 1 } 
@@ -18,4 +18,8 @@ class Item < ApplicationRecord
   validates :shipping_fee_status_id, numericality: { other_than: 1 } 
   validates :prefecture_id,          numericality: { other_than: 1 } 
   validates :scheduled_delivery_id,  numericality: { other_than: 1 } 
+
+  #価格
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range"}
+  validates :price, numericality: { only_integer: true, message: "Half-width number." }
 end
