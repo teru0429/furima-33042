7238172e61bab229795e5bcca418a2756ccfc040
teru0429 <75606622+teru0429@users.noreply.github.com>
2021-01-09 00:22:19 +0900

    Revert "ユーザーの制限"
    This reverts commit 92986d26f63a33b40417f00cdf080a5aeec409be.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,5 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, only: [:edit]
 
   def index
     @items = Item.order("created_at DESC")
@@ -26,12 +25,5 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:name, :info, :price, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :image).merge(user_id: current_user.id)
-  end
-
-  def move_to_index
-    item = Item.find(params[:id])
-    if item.user_id != current_user.id
-      redirect_to action: :index
-    end
   end
 end
