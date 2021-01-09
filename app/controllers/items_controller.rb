53846f89dd1e8f181caba3107ec8ba73b75f4090
teru0429 <75606622+teru0429@users.noreply.github.com>
2021-01-09 14:20:45 +0900

    Revert "編集機能"
    This reverts commit c4192df34a93db7691ccc5eb645ba1dde8767816.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,5 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, only: [:edit]
 
   def index
     @items = Item.order("created_at DESC")
@@ -23,29 +22,8 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  def edit
-    @item = Item.find(params[:id])
-  end
-
-  def update
-    @item = Item.find(params[:id])
-    @item.update(item_params)
-    if @item.save
-      redirect_to root_path(@item)
-    else
-      render :edit
-    end
-  end
-
   private
   def item_params
     params.require(:item).permit(:name, :info, :price, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :image).merge(user_id: current_user.id)
-  end
-
-  def move_to_index
-    item = Item.find(params[:id])
-    if item.user_id != current_user.id
-      redirect_to action: :index
-    end
   end
 end
