94afaf37d29d344458f0e9e6bbc2a296a522408e
teru0429 <75606622+teru0429@users.noreply.github.com>
2021-01-09 16:41:50 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, only: [:edit]
-  before_action :select_item, only: [:edit, :show, :update]
+  before_action :select_item, only: [:edit, :show, :update, :destroy]
+  before_action :move_to_index, only: [:edit, :destroy]
 
   def index
     @items = Item.order("created_at DESC")
@@ -36,7 +36,6 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
     item.destroy
     redirect_to root_path
   end
@@ -47,7 +46,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    item = Item.find(params[:id])
     if item.user_id != current_user.id
       redirect_to action: :index
     end
